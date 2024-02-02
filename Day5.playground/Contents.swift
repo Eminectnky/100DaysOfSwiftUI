252c1523b1f30cf3398f485bfac1d030632f3163
Emine Çetinkaya <eminecetinkayaa98@gmail.com>
2024-02-02 13:53:29 +0300

    How to use switch statements to check multiple conditions

--- Day5.playground/Contents.swift
+++ Day5.playground/Contents.swift
@@ -189,3 +189,40 @@ if score > 9000 {
 }else {
     print("It's not over 9000!")
 }
+
+
+//How to use switch statements to check multiple conditions
+
+let place = "Metropolis"
+
+switch place {
+case "Gotham":
+    print("You're Batman!")
+case "Mega-City One":
+    print("You're Judge Dredd!")
+case "Wakanda":
+    print("You're Black Panther!")
+    
+default:
+    print("Who are you?")
+}
+
+let day = 3
+print("My true love gave to me..")
+
+switch day {
+case 5:
+    print("5 golden rings")
+    fallthrough
+case 4:
+    print("4 calling birds")
+    fallthrough
+case 3:
+    print("3 French hens")
+    fallthrough
+case 2:
+    print("2 turtle doves")
+    fallthrough
+default:
+    print("A partridge in a pear tree")
+}

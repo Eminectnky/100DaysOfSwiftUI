01cea72f4ad72204bba6ba62bd516c39987c7ab9
Emine Çetinkaya <eminecetinkayaa98@gmail.com>
2024-02-17 23:56:32 +0300

    optionals

--- Day14.playground/Contents.swift
+++ Day14.playground/Contents.swift
@@ -50,3 +50,73 @@ func printSquare(of number: Int?) {
     
     print("\(number) x \(number) is \(number * number)")
 }
+
+
+
+//How to unwrap optionals with nil coalescing
+
+let captains = [
+    "Enterprise": "Picard",
+    "Voyager": "Janeway",
+    "Defiant": "Sisko"
+]
+
+
+let new = captains["Serenity", default: "N/A"]
+
+let tvShows = ["Archer", "Babylon 5", "Ted Lasso"]
+let favorite = tvShows.randomElement() ?? "None"
+
+
+struct Book {
+    let title: String
+    let author: String?
+}
+
+let book = Book(title: "Beowulf", author: nil)
+let author = book.author ?? "Anonymous"
+print(author)
+
+
+let input = ""
+let number1 = Int(input) ?? 0
+print(number1)
+
+
+
+//How to handle multiple optionals using optional chaining
+
+let names = ["Arya", "Bran", "Robb", "Sansa"]
+
+let chosen = names.randomElement()?.uppercased() ?? "No one"
+print("Next in line: \(chosen)")
+
+
+struct Book1 {
+    let title: String
+    let author: String?
+}
+
+var book1: Book1? = nil
+let author1 = book1?.author?.first?.uppercased() ?? "A"
+print(author1)
+
+
+
+//How to handle function failure with optionals
+
+enum UserError: Error {
+    case badID, networkFailed
+}
+
+func getUser(id: Int) throws -> String{
+    throw UserError.networkFailed
+}
+
+if let user = try? getUser(id: 23) {
+    print("User: \(user)")
+}
+
+
+let user = (try? getUser(id: 23)) ?? "Anonymous"
+print(user)

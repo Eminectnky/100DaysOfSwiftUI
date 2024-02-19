ac9542fa18f0e7e6eea82dab399b5f505b9367fd
Emine Çetinkaya <eminecetinkayaa98@gmail.com>
2024-02-19 13:00:11 +0300

    Project 1, part one

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,33 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Ron"
+//    @State private var name = "Emine"
+    
+//   @State var topCount = 0
+    
     var body: some View {
         NavigationStack{
-            Form {
-                Section{
-                    Text("Hello, world!")
+            Form{
+                Picker("Select your student", selection: $selectedStudent){
+                    ForEach(students, id: \.self){
+                        Text($0)
+                    }
                 }
-                Section{
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                }
-                Section{
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                }
-                
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
+            
             }
-            .navigationTitle("SwiftUI")
-//            .navigationBarTitleDisplayMode(.inline)
         }
+//        Form{
+//            TextField("Enter your name", text: $name)
+//            Text("Your name is \(name)")
+//        }
+//
+//        Button("Tap Count: \(topCount)"){
+//            self.topCount += 1
+//        }
+        //        NavigationStack{
+        //            Form {
+        //                Section{
+        //                    Text("Hello, world!")
+        //                }
+        //                Section{
+        //                    Text("Hello, world!")
+        //                    Text("Hello, world!")
+        //                }
+        //                Section{
+        //                    Text("Hello, world!")
+        //                    Text("Hello, world!")
+        //                    Text("Hello, world!")
+        //                }
+        //
+        //                Text("Hello, world!")
+        //                Text("Hello, world!")
+        //                Text("Hello, world!")
+        //                Text("Hello, world!")
+        //            }
+        //            .navigationTitle("SwiftUI")
+        ////            .navigationBarTitleDisplayMode(.inline)
+        //        }
+        //    }
     }
 }
+    #Preview {
+        ContentView()
+    }
 
-#Preview {
-    ContentView()
-}

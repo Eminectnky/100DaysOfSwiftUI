37605d8506f4dea9b21a1b4c132a601b57ea0f4e
Emine Çetinkaya <eminecetinkayaa98@gmail.com>
2024-02-23 23:50:00 +0300

    Project 2, part 1

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Emine CETINKAYA on 23.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack(alignment: .top){
+            Text("Hello, world!")
+            Text("This is another text view")
+        }
+    }
+}
+#Preview {
+    ContentView()
+}

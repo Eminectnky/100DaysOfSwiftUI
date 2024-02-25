8dfd31f37d3e9dcf99f43aa9142e5741bd5f5ca2
Emine Çetinkaya <eminecetinkayaa98@gmail.com>
2024-02-25 23:48:09 +0300

    Gradients

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,18 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack{
-            VStack(spacing: 0){
-                Color.red
-                Color.blue
-            }
-           Text("Your content")
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
-              
-        }
+        
+        Text("Your content")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .foregroundStyle(.white)
+            .background(.indigo.gradient)
+//
+//        AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .red], center: .center)
+//
+//        RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
+ 
+            
+//        LinearGradient(stops: [
+//            .init(color: .white, location: 0.45),
+//            .init(color: .black, location: 0.55)
+//        ], startPoint: .top, endPoint: .bottom)
+//   
+        
         .ignoresSafeArea()
+    
+//        ZStack{
+//            VStack(spacing: 0){
+//                Color.red
+//                Color.blue
+//            }
+//           Text("Your content")
+//                .foregroundStyle(.secondary)
+//                .padding(50)
+//                .background(.ultraThinMaterial)
+//              
+//        }
+//        .ignoresSafeArea()
       
         
     }

99604e0637418dbd5e4e17ba3a7870837e6f7864
Emine Çetinkaya <eminecetinkayaa98@gmail.com>
2024-03-26 23:40:16 +0300

    Project 4, part 2

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,21 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
+    @State private var coffeeAmount = 1
     
     var body: some View {
-//        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-//        DatePicker("", selection: $wakeUp, in: Date.now...)
-//            .labelsHidden()
-        Text(Date.now.formatted(date: .long, time: .shortened))
+
+        NavigationStack{
+            VStack{
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+                
+                
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBadTime)
+            }
+        }
     }
     
-    func exampleDates(){
+    func calculateBadTime(){
         
-        let components = Calendar.current.dateComponents([.hour,  .minute], from: .now)
-        let hour = components.hour ?? 0
-        let minute = components.minute ?? 0
     }
 }
 #Preview {

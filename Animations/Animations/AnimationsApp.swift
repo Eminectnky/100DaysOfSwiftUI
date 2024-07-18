33a4ab6e1de73d2c1479119ea16cfb7e2d08eb2c
Emine Çetinkaya <eminecetinkayaa98@gmail.com>
2024-07-19 00:12:00 +0300

    Creating implicit animations

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Emine CETINKAYA on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
